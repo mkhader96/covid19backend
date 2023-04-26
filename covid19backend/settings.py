487f881567d737e46c547a1938ebf2ac7cf34e88
mkhader96 <mkhader996@gmail.com>
2023-04-26 20:15:36 +0000

    Vercel5

--- covid19backend/settings.py
+++ covid19backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,4 +131,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
-STATIC_ROOT = "staticfiles"  # it is used to reform the backend server powered by vercel
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
